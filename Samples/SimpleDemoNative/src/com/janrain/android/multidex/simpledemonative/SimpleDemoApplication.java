9fb19670cfc7d8d601a1357b4dbdc8968dda5d73
pbjanrain <paulbaker@janrain.com>
2016-02-04 12:21:44 -0800

    Commit Verification

--- Samples/SimpleDemoNative/src/com/janrain/android/multidex/simpledemonative/SimpleDemoApplication.java
+++ Samples/SimpleDemoNative/src/com/janrain/android/multidex/simpledemonative/SimpleDemoApplication.java
@@ -35,8 +35,6 @@ package com.janrain.android.multidex.simpledemonative;
 import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
-//import android.support.multidex.MultiDexApplication;
-
 import com.janrain.android.Jump;
 import com.janrain.android.JumpConfig;
 import com.twitter.sdk.android.Twitter;
