768febe17026ab9d2bbbdaf4836ea6eb78d40a23
Miguel Delgado <mdelgado@janrain.com>
2017-09-28 11:40:26 -0500

    MOB-388 Format code.

--- Samples/SimpleDemoNative/jump/src/main/java/com/janrain/android/engage/OpenIDAppAuthGoogle.java
+++ Samples/SimpleDemoNative/jump/src/main/java/com/janrain/android/engage/OpenIDAppAuthGoogle.java
@@ -139,21 +139,21 @@ public class OpenIDAppAuthGoogle extends OpenIDAppAuthProvider {
             @NonNull AuthState authState) {
 
 
-        AuthorizationRequest authRequest = new AuthorizationRequest.Builder(
-                serviceConfig,
-                idp.getClientId(),
-                ResponseTypeValues.CODE,
-                idp.getRedirectUri())
-                .setScope(idp.getScope())
-                .setLoginHint(null)
-                .build();
+        String clientId = idp.getClientId();
+        String code = ResponseTypeValues.CODE;
+        Uri redirectUri = idp.getRedirectUri();
+        AuthorizationRequest authRequest =
+                new AuthorizationRequest.Builder(serviceConfig, clientId, code, redirectUri)
+                        .setScope(idp.getScope())
+                        .setLoginHint(null)
+                        .build();
 
 
         //Intent postAuthIntent = new Intent(context, MyAuthResultHandlerActivity.class);
         //Intent authCanceledIntent = new Intent(context, MyAuthCanceledHandlerActivity.class);
         final JRSession session = JRSession.getInstance();
         session.setCurrentlyAuthenticatingOpenIDAppAuthProvider(this);
-        OpenIDAppAuthTokenActivity ta = new OpenIDAppAuthTokenActivity();
+        // OpenIDAppAuthTokenActivity ta = new OpenIDAppAuthTokenActivity();
         LogUtils.logd(TAG, "Making auth request to " + serviceConfig.authorizationEndpoint);
         Context appContext = session.getCurrentOpenIDAppAuthActivity().getBaseContext();
 
@@ -163,12 +163,19 @@ public class OpenIDAppAuthGoogle extends OpenIDAppAuthProvider {
 
         mAuthService.performAuthorizationRequest(
                 authRequest,
-                ta.createPostAuthorizationIntent(
+                OpenIDAppAuthTokenActivity.createPostAuthorizationIntent(
                         appContext,
                         authRequest,
                         serviceConfig.discoveryDoc,
-                        authState),
-                PendingIntent.getActivity(appContext, 0, cancelIntent, PendingIntent.FLAG_UPDATE_CURRENT));
+                        authState
+                ),
+                PendingIntent.getActivity(
+                        appContext,
+                        0,
+                        cancelIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                )
+        );
 
     }
 
@@ -195,8 +202,11 @@ public class OpenIDAppAuthGoogle extends OpenIDAppAuthProvider {
                             idp.setClientId(registrationResponse.clientId);
                             LogUtils.logd(TAG, "Registration request complete successfully");
                             // Continue with the authentication
-                            makeAuthRequest(registrationResponse.request.configuration, idp,
-                                    new AuthState((registrationResponse)));
+                            makeAuthRequest(
+                                    registrationResponse.request.configuration,
+                                    idp,
+                                    new AuthState(registrationResponse)
+                            );
                         }
                     }
                 });
