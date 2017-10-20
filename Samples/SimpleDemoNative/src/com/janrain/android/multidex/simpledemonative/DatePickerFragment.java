7b31e77405cfcf8b48f967edbe04a5b0e55ae0ea
Miguel Delgado <mdelgado@janrain.com>
2017-10-20 02:21:00 -0500

    MOB-389 Add missing fields to native demo app,

---
+++ Samples/SimpleDemoNative/src/com/janrain/android/multidex/simpledemonative/DatePickerFragment.java
@@ -0,0 +1,57 @@
+package com.janrain.android.multidex.simpledemonative;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
+
+    public static DatePickerFragment getInstance(Date currentDate) {
+        final DatePickerFragment fragment = new DatePickerFragment();
+        final Bundle arguments = new Bundle(1);
+
+        final Calendar c = Calendar.getInstance();
+        if (currentDate != null) {
+            c.setTime(currentDate);
+        }
+
+        arguments.putInt("year", c.get(Calendar.YEAR));
+        arguments.putInt("month", c.get(Calendar.MONTH));
+        arguments.putInt("day", c.get(Calendar.DAY_OF_MONTH));
+        fragment.setArguments(arguments);
+
+        return fragment;
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener;
+
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current date as the default date in the picker
+        final Bundle arguments = getArguments();
+        int year = arguments.getInt("year");
+        int month = arguments.getInt("month");
+        int day = arguments.getInt("day");
+
+        // Create a new instance of DatePickerDialog and return it
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        if (dateSetListener != null) {
+            dateSetListener.onDateSet(view, year, month, dayOfMonth);
+        }
+    }
+
+    public void setOnDateSetListener(DatePickerDialog.OnDateSetListener listener) {
+        dateSetListener = listener;
+    }
+}
